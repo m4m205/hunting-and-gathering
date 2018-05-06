943fd8664d331df01511bee2c61682c768f649c0
Mahmoud <m4m205@gmail.com>
2018-05-06 14:08:42 +0200

    set the port

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '3100';
 $db['default']['database'] = 'connected';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
